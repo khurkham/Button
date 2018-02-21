1b692745da631c100b68e27ae1db7265736140a7
khurkham <30972820+khurkham@users.noreply.github.com>
2018-02-21 23:18:32 +0700

    Initial commit

---
+++ app/src/main/java/khurkham/com/android/custombuttons/MainActivity.java
@@ -0,0 +1,40 @@
+package khurkham.com.android.custombuttons;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import khurkham.com.android.custombuttons.custombuttons.R;
+
+public class MainActivity extends AppCompatActivity {
+    Button rect, round, oval, circle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rect = (Button)findViewById(R.id.rect);
+        round = (Button)findViewById(R.id.rou);
+        oval = (Button)findViewById(R.id.oval);
+        circle = (Button)findViewById(R.id.cir);
+
+        rect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Rectangle Button clicked!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        round.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Round Button clicked!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+}
+
